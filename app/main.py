9def2a5ec68e03fb80b2a6011218342b9189500c
Luis Kirby M. Salva <luiskirby.salva@gmail.com>
2022-01-03 10:06:39 +0800

    Use SQLAlchemy for get_all_posts & create_post route. Also used dictionary unpacking for creating a new post.

--- app/main.py
+++ app/main.py
@@ -42,21 +42,31 @@ def root():
 
 # Get posts
 @app.get("/posts", status_code=status.HTTP_200_OK)
-def get_posts():
-    cursor.execute("SELECT * FROM posts")
-    posts = cursor.fetchall()
+def get_posts(db: Session = Depends(get_db)):
+    # # psycopg2 code
+    # cursor.execute("SELECT * FROM posts")
+    # posts = cursor.fetchall()
+
+    posts = db.query(models.Post).all()
     return {"all_posts": posts}
 
 
 # Create posts
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
-def create_post(post: Post):
-    cursor.execute(
-        "INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *",
-        (post.title, post.content, post.published),
-    )
-    new_post = cursor.fetchone()
-    connection.commit()
+def create_post(post: Post, db: Session = Depends(get_db)):
+    # # psycopg2 code
+    # cursor.execute(
+    #     "INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *",
+    #     (post.title, post.content, post.published),
+    # )
+    # new_post = cursor.fetchone()
+    # connection.commit()
+
+    new_post = models.Post(**post.dict())
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
+
     return {"new_post": new_post}
 
 
@@ -100,4 +110,6 @@ def delete_post(id: int):
 
 @app.get("/sqlalchemy")
 def test_posts(db: Session = Depends(get_db)):
-    return {"status": "success"}
+    posts = db.query(models.Post).all()
+
+    return {"data": posts}
